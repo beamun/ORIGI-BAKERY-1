9e317c75a98f298451db451cdfab750cdddd1643
artapln <50373007+artapln@users.noreply.github.com>
2020-11-19 19:42:31 +0700

    Add files via upload

---
+++ ORIGI-BAKERY-main/origibakery/src/main/java/th/ac/kmitl/origibakery/controller/HomeController.java
@@ -0,0 +1,15 @@
+package th.ac.kmitl.origibakery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String getHomePage(Model model) {
+        // return home.html
+        return "home";
+    }
+}
---
+++ ORIGI-BAKERY-main/origibakery/src/test/java/th/ac/kmitl/origibakery/OrigibakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.kmitl.origibakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrigibakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
